9ea89659f70426c88e2a231d872b3bf536c72f05
garzuzo <garzuzo@garzuzo-MS-7641>
2018-03-02 00:30:16 -0500

    nfs

--- JIM The Ripper/src/Others/NeedForSpeed.java
+++ JIM The Ripper/src/Others/NeedForSpeed.java
@@ -12,21 +12,24 @@ public class NeedForSpeed {
 
 		
 		int[] arr=new int[3];
-		binarySearch(arr, 0, arr.length-1, 2);
+		int min=Integer.MAX_VALUE;
+		for (int i = 0; i < arr.length; i++) {
+			min=Math.min(min, arr[i]);
+		}
+		binarySearch(arr, 0, min, 100000001);
 	}
-	
-	
-	static long binarySearch(int[]arr,int l,int r,int x) {
-		
-		if(r>=l) {
-		int mid=l+(r-1)/2;
-		if(arr[mid]==x)
-return mid;
-		else if(arr[mid]>x)
-			return binarySearch(arr, l, mid-1, x);
-		else
-			return binarySearch(arr, mid+1, r, x);
-		
+
+	static long binarySearch(int[] arr, int l, int r, int x) {
+
+		if (r >= l) {
+			int mid = l + (r - 1) / 2;
+			if (arr[mid] == x)
+				return mid;
+			else if (arr[mid] > x)
+				return binarySearch(arr, l, mid - 1, x);
+			else
+				return binarySearch(arr, mid + 1, r, x);
+
 		}
 		return -1;
 	}
